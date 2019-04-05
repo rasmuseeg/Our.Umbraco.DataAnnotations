eadee469856e801596bd6d6e05e5fc4ee00f75bf
Rasmus Eeg Møller <rasmuseeg@outlook.com>
2019-04-06 00:10:39 +0200

    Push nupkg as artifact

--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -2,3 +2,6 @@ $nuget = './src/.nuget/nuget.exe';
 $csproj = 'src/Our.Umbraco.DataAnnotations/Our.Umbraco.DataAnnotations.csproj';
 
 & $nuget @("pack", $csproj, '-properties', 'Configuration=Release')
+
+# Push all newly created .nupkg files as Appveyor artifacts for later deployment.
+Get-ChildItem .\*.nupkg | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
