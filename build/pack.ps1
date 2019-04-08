c03f3a7787ffc9379c09742a88a476de8f551e69
Rasmus Eeg Møller <rasmuseeg@outlook.com>
2019-04-08 08:52:12 +0200

    Changed path for grunt execute

--- build/pack.ps1
+++ build/pack.ps1
@@ -4,15 +4,14 @@ $csproj = 'src/Our.Umbraco.DataAnnotations/Our.Umbraco.DataAnnotations.csproj';
 & $nuget @("pack", $csproj, '-properties', 'Configuration=Release')
 
 # Run Grunt
+Set-Location -Path .\build\
 grunt
 
-
-
 # Push all newly created .nupkg files as Appveyor artifacts for later deployment.
-Get-ChildItem .\build\release\nuget\*.nupkg | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+Get-ChildItem .\release\nuget\*.nupkg | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 
 # Push umbraco package
-Get-ChildItem .\build\release\umbraco\*.zip | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+Get-ChildItem .\release\umbraco\*.zip | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 
 # Push zip
-Get-ChildItem .\build\release\zip\*.zip | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+Get-ChildItem .\release\zip\*.zip | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
