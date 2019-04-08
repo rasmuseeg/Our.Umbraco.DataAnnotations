8520439e98cf8e22a4ecfd3d565c2164332f0d46
Rasmus Eeg Møller <rasmuseeg@outlook.com>
2019-04-08 08:57:29 +0200

    Fixed grunt build path

--- build/pack.ps1
+++ build/pack.ps1
@@ -1,17 +1,17 @@
 $nuget = './src/.nuget/nuget.exe';
 $csproj = 'src/Our.Umbraco.DataAnnotations/Our.Umbraco.DataAnnotations.csproj';
+$releasePath = ".\build\release";
 
 & $nuget @("pack", $csproj, '-properties', 'Configuration=Release')
 
 # Run Grunt
-Set-Location -Path .\build\
-grunt
+grunt --base build
 
 # Push all newly created .nupkg files as Appveyor artifacts for later deployment.
-Get-ChildItem .\release\nuget\*.nupkg | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+Get-ChildItem "$releasePath\nuget\*.nupkg" | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 
 # Push umbraco package
-Get-ChildItem .\release\umbraco\*.zip | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+Get-ChildItem "$releasePath\umbraco\*.zip" | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 
 # Push zip
-Get-ChildItem .\release\zip\*.zip | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+Get-ChildItem "$releasePath\zip\*.zip" | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
