cd2864d821724ad5a87e7973ab45008742c5876c
Rasmus Eeg Møller <rasmuseeg@outlook.com>
2019-04-08 08:58:03 +0200

    Removed nuget from ps script (grunt-nuget)

--- build/pack.ps1
+++ build/pack.ps1
@@ -1,9 +1,3 @@
-$nuget = './src/.nuget/nuget.exe';
-$csproj = 'src/Our.Umbraco.DataAnnotations/Our.Umbraco.DataAnnotations.csproj';
-$releasePath = ".\build\release";
-
-& $nuget @("pack", $csproj, '-properties', 'Configuration=Release')
-
 # Run Grunt
 grunt --base build
 
