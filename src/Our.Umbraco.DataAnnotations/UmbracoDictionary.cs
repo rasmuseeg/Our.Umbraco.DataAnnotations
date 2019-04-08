40dcffa0ffb1aa496ba95217c4dd500dfb012b0d
Rasmus Eeg Møller <rasmuseeg@outlook.com>
2019-04-08 10:08:17 +0200

    Another fix to dictionary (-.-)

--- src/Our.Umbraco.DataAnnotations/UmbracoDictionary.cs
+++ src/Our.Umbraco.DataAnnotations/UmbracoDictionary.cs
@@ -23,7 +23,7 @@ namespace Our.Umbraco.DataAnnotations
             string key = Helper.GetDictionaryValue(dictionaryKey);
             if (!string.IsNullOrEmpty(key))
                 return key;
-            return string.Format(Config.DictionaryFallbackFormat, key); // Fallback with the key name
+            return string.Format(Config.DictionaryFallbackFormat, dictionaryKey); // Fallback with the key name
         }
     }
 }
